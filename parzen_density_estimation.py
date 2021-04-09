3ef0cc09f2fe2b8a1b9f0ab5d7da682fcd1530cd
Christopher Haen <2007bucks@gmail.com>
2021-04-09 17:22:26 -0400

    y_axis_name

--- parzen_density_estimation.py
+++ parzen_density_estimation.py
@@ -11,10 +11,12 @@ from pandas.core.frame import DataFrame
 from scipy.stats import gaussian_kde
 
 
+y_axis_name = 'NUMBER OF PERSONS KILLED'
+
 def clean():
   data = pandas.read_csv('Motor_Vehicle_Collisions_-_Crashes.csv')
 
-  data = data[['ZIP CODE', 'NUMBER OF PERSONS INJURED', 'CRASH TIME']]
+  data = data[['ZIP CODE', y_axis_name, 'CRASH TIME']]
   # Reduce rows for testing
   # data = data.head(100000)
 
@@ -22,7 +24,7 @@ def clean():
 
   data['TIMESTAMP'] = data['CRASH TIME'].apply(lambda s: float(s.split(':')[0]) + int(s.split(':')[1]) * (1 / 60))
   data['ZIP CODE'] = pandas.to_numeric(data['ZIP CODE'], errors='coerce', downcast='integer')
-  data['NUMBER OF PERSONS INJURED'] = pandas.to_numeric(data['NUMBER OF PERSONS INJURED'], errors='coerce', downcast='integer')
+  data[y_axis_name] = pandas.to_numeric(data[y_axis_name], errors='coerce', downcast='integer')
 
   data.dropna(axis=0, inplace=True)
 
@@ -32,7 +34,7 @@ def clean():
 clean_data = clean()
 #two_dim_kde(clean_data.to_numpy())
 
-ax1 = clean_data.plot(kind='scatter', x='TIMESTAMP', y='NUMBER OF PERSONS INJURED', label='scatter')
+ax1 = clean_data.plot(kind='scatter', x='TIMESTAMP', y=y_axis_name, label='scatter')
 ax1.set_xticks(np.arange(24))
 ax2 = clean_data['TIMESTAMP'].plot(kind='kde', color='red', label='kde', secondary_y=True)
 ax1.set_ylabel('')
