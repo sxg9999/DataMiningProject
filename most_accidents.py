876dea5f908a8372fdce5ff129cc25829b7e1a53
aconncep <austincepalia@gmail.com>
2021-04-09 21:49:08 -0400

    yea this probably works

--- most_accidents.py
+++ most_accidents.py
@@ -4,17 +4,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('cleaned_data_month_8.csv')
-
+df = pd.read_csv('Motor_Vehicle_Collisions_-_Crashes.csv')
+df = df[pd.to_datetime(df['CRASH DATE']) >= pd.to_datetime('2020-01-01')]
+df = df[pd.to_datetime(df['CRASH DATE']) < pd.to_datetime('2021-01-01')]
 accident_counts = df['BOROUGH'].value_counts()
 accident_counts.plot(kind="bar")
 
-plt.title("Accidents by Borough - 08/2012")
+plt.title("Accidents by Borough - 2020")
 plt.xlabel("Borough")
 plt.ylabel("Number of accidents")
 plt.gcf().subplots_adjust(bottom=0.30)
 plt.show()
 
-
-#print(df.BOROUGH.unique())
 
